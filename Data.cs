32b474395707dca96f1a7ab89d515a33a3a0bee7
Leonhard Seidel <leonhardseidel@gmx.de>
2021-10-29 12:25:35 +0200

    init

---
+++ Data.cs
@@ -0,0 +1,12 @@
+﻿namespace HPspeed
+{
+    public struct Data
+    {
+#pragma warning disable IDE0044 // Add readonly modifier
+        public float DeltaTime;
+        public bool LoadingScreen;
+        public bool GameMain;
+        public bool MainMenu;
+#pragma warning restore IDE0044 // Add readonly modifier
+    }
+}
---
+++ HPspeed.cs
@@ -0,0 +1,41 @@
+﻿using MelonLoader;
+using System.IO.MemoryMappedFiles;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HPspeed
+{
+    public class HPspeed : MelonMod
+    {
+        private Scene scene = SceneManager.GetActiveScene();
+        public Data SharedData;
+        private MemoryMappedFile MemoryFile = MemoryMappedFile.CreateOrOpen("HousePartyMemoryFile",1024);
+        private MemoryMappedViewAccessor Accessor;
+
+
+        public override void OnApplicationStart()
+        {
+            Accessor = MemoryFile.CreateViewAccessor();
+        }
+        public override void OnApplicationQuit()
+        {
+            Accessor.Dispose();
+            MemoryFile.Dispose();
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+
+        }
+
+        public override void OnUpdate()
+        {
+            SharedData.DeltaTime = Time.deltaTime;
+            SharedData.GameMain = scene.name == "GameMain";
+            SharedData.MainMenu = scene.name == "MainMenu";
+            SharedData.LoadingScreen = scene.name == "LoadingScreen";
+
+            Accessor.Write(0, ref SharedData);
+        }
+    }
+}
